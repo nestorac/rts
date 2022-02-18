dcec4927c2a1d22fbd631fa71fa115c7219ca4c2
Néstor Amigo Cairo <nestorac@gmail.com>
2022-02-18 11:31:53 +0100

    Trasteando

--- script.gd
+++ script.gd
@@ -9,8 +9,11 @@ func _ready():
 func _input(event):
 	if (event is InputEventMouseButton and event.pressed and event.button_index == 1):
 		var camera = $Camera
+		var ray_lenght = guerrero.distance_to(camera)
+
 		var mouse_pos = get_viewport().get_mouse_position()
 		var from = camera.project_ray_origin(mouse_pos)
-		var to = from + camera.project_ray_normal(mouse_pos)
+		var to = from + camera.project_ray_normal(mouse_pos) * ray_lenght
 		# Apply the position to whatever object you want
-		guerrero.global_transform.origin = guerrero.global_transform.origin.linear_interpolate(to, 0.3)
+		#guerrero.global_transform.origin = guerrero.global_transform.origin.linear_interpolate(to, 0.3)
+		guerrero.global_transform.origin = to;

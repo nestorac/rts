3bdf452de66d278c0d6593c09aff0dd5a55fbb3b
Néstor <nestorac@gmail.com>
2022-03-18 18:14:36 +0100

    The arrow now hurts and the floor is a grid.

--- arrow.gd
+++ arrow.gd
@@ -4,6 +4,7 @@ extends Spatial
 # var a = 2
 # var b = "text"
 
+export var speed = 15
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,9 +12,13 @@ func _ready():
 	
 func _physics_process(delta):
 	var forward_vector = -global_transform.basis.z
-	global_translate(forward_vector * 5 * delta)
+	global_translate(forward_vector * speed * delta)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Timer_timeout():
+	queue_free()

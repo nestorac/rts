d0861dadfaf7fdcedc33b0d935d6c12837b43eec
Néstor <nestorac@gmail.com>
2022-06-04 18:49:05 +0200

    New menu.

--- Warrior.gd
+++ Warrior.gd
@@ -7,6 +7,7 @@ var arrow_resource = preload("res://arrow.tscn")
 
 # Stats
 var health = 3
+var attack = 2
 
 # For pathfinding
 var path = []
@@ -19,20 +20,25 @@ onready var anim = get_node("AnimationPlayer")
 onready var hand_position = $"Arm/HandPosition"
 onready var selection_ring = $"SelectionRing"
 
+onready var unit_stats_ui = $"UnitStatsUI"
+onready var health_label = $"UnitStatsUI/BackgroundRect/VBoxContainer/HealthLabel"
+onready var attack_label = $"UnitStatsUI/BackgroundRect/VBoxContainer/AttackLabel"
+onready var speed_label = $"UnitStatsUI/BackgroundRect/VBoxContainer/SpeedLabel"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	health_label.text = "health: " + str(health)
+	attack_label.text = "attack: " + str(attack)
+	speed_label.text = "speed: " + str(SPEED)
 
 func _physics_process(delta):
-	if health <= 0:
-		visible = false
-
 	if path_index < path.size():
 		var move_vector = path[path_index] - global_transform.origin
 		
 		if move_vector.length() < 1:
 			path_index += 1
 		else:
+			look_at(path[path_index],Vector3.UP)
 			move_and_slide(move_vector.normalized() * SPEED * delta, Vector3.UP)
 
 	if Input.is_action_just_pressed("ui_accept"):
@@ -51,12 +57,17 @@ func move_to(target_pos):
 	
 func select():
 	selection_ring.show()
+	unit_stats_ui.show()
 
 func deselect():
 	selection_ring.hide()
+	unit_stats_ui.hide()
 
-func get_damage(damage):
+func get_damage(damage):	
 	health -= damage
+	health_label.text = "health: " + str(health)
+	if health <= 0:
+		visible = false
 
 func _on_HurtBox_area_entered(area):
 	if area.is_in_group("Weapon"):
